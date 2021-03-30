ed94a539afb420b2aae56f6ee2fc9c2a6b3a4491
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-30 14:05:53 -0700

    Users Router created

--- server/routes/apiUsers.js
+++ server/routes/apiUsers.js
@@ -1,1 +1,27 @@
 const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.get('/', userController.getAllUsers, (req, res) => {
+  res.status(200).json(res.locals.users);
+
+});
+
+router.get('/:email', userController.getUserByEmail, (req, res) => {
+  res.status(200).json(res.locals.user);
+});
+
+router.post('/create', userController.createUser, (req, res) => {
+  res.status(200).json(res.locals.userId);
+});
+
+router.patch('/:email', userController.updateUser, (req, res) => {
+  res.status(200).json(res.locals.user);
+});
+
+router.patch('/close/:email', userController.closeUser, (req, res) => {
+  res.status(200).json({message: 'user is now inactive'});
+});
+
+
+module.exports = router;
