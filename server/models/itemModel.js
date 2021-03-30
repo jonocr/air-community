83734a133404aed12dc70868345dfd686e1106e8
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-30 15:38:39 -0700

    Item Model Added

--- server/models/itemModel.js
+++ server/models/itemModel.js
@@ -6,7 +6,7 @@ const itemSchema = new mongoose.Schema({
   name: {type: String, required: true},
   description: {type: String, required: true},
   ownerId: {type: String, required: true},
-  renterId: {type: String, required: true},
+  renterId: {type: String},
   cost: {type: Number, required: true},
   status: {type: String, required: true, default: 'available'}
 });
