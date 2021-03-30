a43c313d4a58d0748baf102225ae7da5c7ad2e3c
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-30 11:30:18 -0700

    Init project, setup server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+
+const PORT = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const mongoURI = 'mongodb+srv://codesmith:s0lOprojec7@cluster0.l6e2x.mongodb.net/air_community?retryWrites=true&w=majority';
+mongoose.connect(mongoURI);
+
+app.use('/', (req, res) => {
+  res.status(200).send('Houston, the satellite is in orbit!');
+});
+
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.status(404).send('This is not the page you\'re looking for...'));
+
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
