112a8febeab7e950960a9515057e66259f4e0689
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-30 14:27:01 -0700

    Item Model created

---
+++ server/models/itemModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const schema = mongoose.Schema;
+
+
+const itemSchema = new mongoose.Schema({
+  name: {type: String, required: true},
+  description: {type: String, required: true},
+  ownerId: {type: String, required: true},
+  renterId: {type: String, required: true},
+  cost: {type: Number, required: true},
+  status: {type: String, required: true, default: 'available'}
+});
+
+module.exports = mongoose.model('Item', itemSchema);
