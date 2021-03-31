254e8a6f0a9433cdedda94de378c8f72e21f09fe
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-31 14:21:28 -0700

    Created component ItemsList

---
+++ client/components/ItemsList.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const ItemsList = (props) => {
+	return (
+		<div>
+			<h3>ItemsList</h3>
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th scope="col">#</th>
+						<th scope="col">Title</th>
+						<th scope="col">Description</th>
+						<th scope="col">Cost</th>
+						<th scope="col">Status</th>
+					</tr>
+				</thead>
+				<tbody>
+					{props.data.map((item, index) => (
+						<tr
+							key={index} 
+							onClick={(e) => props.onClick(item)}
+						>
+							<th scope="row">{index}</th>
+							<td>{item.name}</td>
+							<td>{item.description}</td>
+							<td>{item.cost}</td>
+							<td>{item.status}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
+		</div>
+	);
+};
+
+export default ItemsList;
