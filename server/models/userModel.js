eca75361303aed530f2ea6abbeacde34529ba67e
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-31 21:56:49 -0700

    Login route now creifies for auth and creates cookie with ssid

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const schema = mongoose.Schema;
 
 //Stretch Feature
-// const SALT_WORK_FACTOR = 10;
-// const bcrypt = require('bcryptjs');
+const SALT_WORK_FACTOR = 10;
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   email: {type: String, required: true, unique: true},
@@ -13,5 +13,17 @@ const userSchema = new mongoose.Schema({
   location: {type: String, required: true},
   status: {type: String, required: true, default: 'active'}
 });
+
+userSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(this.password, SALT_WORK_FACTOR);
+  return next();
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 module.exports = mongoose.model('User', userSchema);
--- server/routes/apiUsers.js
+++ server/routes/apiUsers.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const cookieController = require('../controllers/cookieController');
+
+router.post('/login', userController.verifyUser, cookieController.setSSIDCookie, (req, res) => {
+  //TODO: Clean res obj before sending back
+  console.log("User: ", res.locals.user);
+  const user = { id: res.locals.user._id, firstName: res.locals.user.firstName, lastName: res.locals.user.lastName, email: res.locals.user.email, location: res.locals.user.location }
+  res.status(200).json(user);
+
+});
 
 router.get('/', userController.getAllUsers, (req, res) => {
   res.status(200).json(res.locals.users);
