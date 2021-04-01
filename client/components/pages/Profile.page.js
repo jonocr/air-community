ee99883a36c2c978574ba6d9a04d352d5fa43b9b
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-31 21:52:12 -0700

    Added Menu and checks if user is logeg in

--- client/components/pages/Profile.page.js
+++ client/components/pages/Profile.page.js
@@ -1,45 +1,57 @@
 import React, { useContext, useEffect, useState } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-
-// import SideMenu from "../../layout/SideMenu";
-// import TopBar from "../../layout/TopBar";
-
+import { BrowserRouter as Router, Route, Link, useHistory } from "react-router-dom";
+import { AuthContext } from "../contexts/Auth.context";
+import Menu from '../layout/Menu.layout'
 
 const UserProfilePage = (props) => {
+  const { user, setUser} = useContext(AuthContext);
+  const history = useHistory();
 	
+	useEffect(() => {
+    if (!user) history.push('/');
+	}, []);
+
 	return (
 		<div className="profile-page">
-			<div>
-				{/* <SideMenu css={closeCss}></SideMenu>
-				<TopBar onClick={clickToggle} css={closeCss}></TopBar> */}
-				<div className="container">
-					<div className="dashboard-bar dashboard">My Profile</div>
-					<div className="dashboard-main dashboard">
-						<div className="card">
-							<div className="card-header">
-								<div className="profile-picture-card ">
-									
+		<Menu/>
+			{user ? (
+				<div>
+					<div className="container">
+						<div className="dashboard-bar dashboard">My Profile</div>
+						<div className="dashboard-main dashboard">
+							<div className="card">
+								<div className="card-header">
+									<div className="profile-picture-card ">
+										<img
+											src='https://randomuser.me/api/portraits/lego/7.jpg'
+											className="profile-picture rounded-circle"
+											alt="Profile Flag">
+										</img>
+									</div>
+									<div className="profile-name-card">
+										<div className="username">{user.firstName}</div>
+										<div className="role"> {user.lastName}</div>
+									</div>
 								</div>
-								<div className="profile-name-card">
-									<div className="username">userName</div>
-									<div className="role"> department</div>
-								</div>
-							</div>
 
-							<div>
-								<ul className="list-group list-group-flush">
-									<li className="list-group-item">Username: </li>
-									<li className="list-group-item">Role: </li>
-									<li className="list-group-item">Email:</li>
-									<li className="list-group-item">
-										User since: 
-									</li>
-								</ul>
+								<div>
+									<ul className="list-group list-group-flush">
+										<li className="list-group-item">First Name: {user.firstName}</li>
+										<li className="list-group-item">Last Name: {user.lastName}</li>
+										<li className="list-group-item">Email: {user.email}</li>
+										<li className="list-group-item">Location: {user.location}</li>
+										<li className="list-group-item">
+											<a className="btn btn-primary" href="#" role="button">Save</a> 
+											<a className="btn btn-light btn-delete" href="#" role="button">Delete</a>
+										</li>
+									</ul>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+				):(<div>Loading...</div>)
+			}
 		</div>
 	);
 };
