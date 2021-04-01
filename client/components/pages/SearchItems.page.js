5e188110a283f599e9c41054f83b95a9fdd1e9bb
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-31 21:53:57 -0700

    Auth added

--- client/components/pages/SearchItems.page.js
+++ client/components/pages/SearchItems.page.js
@@ -1,47 +1,61 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useHistory } from 'react-router-dom';
+import { AuthContext } from "../contexts/Auth.context";
 import ItemsList from '../ItemsList';
+import Menu from '../layout/Menu.layout'
 
 const ItemSearchPage = (props) => {
-
-	const [closeCss, setCloseCss] = useState("");
+	const abortController = new AbortController();
+  const { user, setUser} = useContext(AuthContext);
 	const [items, setItems] = useState([]);
-	const [itemId, setItemId] = useState('');
   const history = useHistory();
   const handleClick = (item) => {
-    console.log("Onclick: ", item._id);
     history.push({
       pathname:`/item-detail/${item._id}`,
       state: { item: item }
     }
   )};
-	// const [loaded, setLoaded] = useState(false);
 
 
 	useEffect(() => {
-    
-    fetch('/items')
-      .then(response => response.json())
-      .then(items => {
-        setItems(items);
-        // setLoaded(true);
-      });
-    console.log("UseEffect: ", items);
+    if (!user) {
+      history.push('/');
+      } else {
+      fetch('/items')
+        .then(response => response.json())
+        .then(items => {
+          setItems(items);
+          // setLoaded(true);
+        });
+    }
+
+		return function cleanup() {
+			abortController.abort();
+		};
 	}, []);
 
 	return (
-		<div>
-      <h2>Search Items</h2>
-			{/* <SideMenu css={closeCss}></SideMenu>
+		<div>	
+		<Menu/>
+      {/* <SideMenu css={closeCss}></SideMenu>
 			<TopBar onClick={clickToggle} css={closeCss}></TopBar> */}
-			<div className="container">
-				<div className="dashboard-bar dashboard">Item Search</div>
+      { user ? (
+      <div>
+        <h2>Search Items</h2>
+      
+        <div className="container">
+          <div className="dashboard-bar dashboard">Item Search</div>
 
-				<div className="dashboard-main dashboard">
-        <ItemsList data={items} onClick={handleClick}></ItemsList>
-				
-				</div>
-			</div>
+          <div className="dashboard-main dashboard">
+          <ItemsList data={items} onClick={handleClick}></ItemsList>
+          
+          </div>
+        </div>
+      </div>
+      ) :
+      (<div>loading...</div>)
+      }
+      
 		</div>
 	);
 };
