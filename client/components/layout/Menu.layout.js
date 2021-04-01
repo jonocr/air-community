21d3ebfcae47cbe29ead97f5dac18862572da54b
Jonathan Calvo <jono.calvo@gmail.com>
2021-04-01 16:49:16 -0700

    Menu now handles logout

--- client/components/layout/Menu.layout.js
+++ client/components/layout/Menu.layout.js
@@ -1,14 +1,25 @@
 import React, { useState, useEffect, useContext } from "react";
-// import AuthContext from "../utils/AuthContext";
+import { useHistory } from 'react-router-dom';
+import { AuthContext } from "../contexts/Auth.context";
 import { Link } from "react-router-dom";
 
 const Menu = (props) => {
+  const { user, setUser} = useContext(AuthContext);
+  const history = useHistory();
 
+  useEffect(() => {
+    if (!user) history.push('/');
+	});
+
+  const logOut = () => {
+    setUser(null);
+  }
 	return (		
     <nav className="nav">
       <Link className="nav-link" to='/my-profile'>Profile</Link>
+      <Link className="nav-link" to='/create-item'>Add Item</Link>
       <Link className="nav-link" to='/search-items'>Search Items</Link>
-      <Link className="nav-link" to='/search-items'>Log Out</Link>
+      <Link className="nav-link" to='/search-items' onClick={logOut}>Log Out</Link>
     </nav>
 	);
 };
