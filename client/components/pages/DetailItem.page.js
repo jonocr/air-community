0bc23ad97bd291c7b6e99a2845e6b7519e3ff50c
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-31 21:50:56 -0700

    Added Menu to page

--- client/components/pages/DetailItem.page.js
+++ client/components/pages/DetailItem.page.js
@@ -1,41 +1,18 @@
 import React, { useState, useEffect, useContext } from "react";
-import { useLocation } from "react-router-dom";
-import ItemsList from '../ItemsList';
+import Menu from '../layout/Menu.layout'
 
 const DetailItemPage = (props) => {
-  const location = useLocation();
-
-	const [closeCss, setCloseCss] = useState("");
-	const [items, setItems] = useState([]);
-	// const [loaded, setLoaded] = useState(false);
-
-
-	useEffect(() => {
-    
-    // fetch(`/items/`)
-    //   .then(response => response.json())
-    //   .then(items => {
-    //     setItems(items);
-    //     // setLoaded(true);
-    //   });
-    console.log("Item Detail UseEffect: ", location.state);
-	}, []);
-
-  useEffect(() => {
-    console.log("Render: ");
-	});
 
 	return (
 		<div>
+		<Menu/>
       <h2>Item Detail</h2>
-			{/* <SideMenu css={closeCss}></SideMenu>
-			<TopBar onClick={clickToggle} css={closeCss}></TopBar> */}
 			<div className="container">
 				<div className="dashboard-bar dashboard">Item Detail</div>
 
 				<div className="dashboard-main dashboard">
         
-        <div> Title: {location.state.item.title}</div>
+        <div> Title: {location.state.item.name}</div>
         <div> Description: {location.state.item.description}</div>
         <div> Cost: {location.state.item.cost}</div>
         <div> Status: {location.state.item.status}</div>
