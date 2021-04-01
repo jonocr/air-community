082aa95b08b0fcc2d899acb5779e0f678a7e487e
Jonathan Calvo <jono.calvo@gmail.com>
2021-03-31 21:46:31 -0700

    Move menu out of App to specific Pages

--- client/components/App.js
+++ client/components/App.js
@@ -1,55 +1,58 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useMemo } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
+import { AuthContext } from './contexts/Auth.context';
 import LoginPage from './pages/Login.page';
 import UserProfilePage from './pages/Profile.page';
 import SearchItems from './pages/SearchItems.page';
 import DetailItem from './pages/DetailItem.page';
+import Menu from './layout/Menu.layout';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    // this.handleClick = this.handleClick.bind(this);
-    // this.state = getInitialState();
-  }
+function App() {
+  const [user, setUser] = useState(null);
+  const providerUser = useMemo(() => ({user, setUser}), [user, setUser]);
+  // constructor(props) {
+  //   super(props);
+  //   // this.handleClick = this.handleClick.bind(this);
+  //   // this.state = getInitialState();
+  // }
   
-  handleClick() {
 
-  }
 
-  render() { 
+ 
 
     return (
       <div>
         <Router>
-        <ul>
-          <li>
-            <Link to='/my-profile'>Profile</Link>
-          </li>
-          <li>
-            <Link to='/search-items'>Search Items</Link>
-          </li>
-        </ul>
-					<Route component={LoginPage} exact path="/#/login" />
-					{/* <Route component={LoginPage} exact path="/" /> */}
-					{/* <Route component={UserProfilePage} exact path="/my-profile" /> */}
-          <Route path='/my-profile'>
-            <UserProfilePage />
-          </Route>
-          <Route path='/search-items'>
-            <SearchItems />
-          </Route>
-          <Route path='/item-detail/:itemId'>
-            <DetailItem />
-          </Route>
-        {/* <h1>
-          APP.JS
-        </h1>
-        <LoginPage/> */}
+          <AuthContext.Provider value={providerUser}>
+            {/* <ul>
+              <li>
+                <Link to='/my-profile'>Profile</Link>
+              </li>
+              <li>
+                <Link to='/search-items'>Search Items</Link>
+              </li>
+            </ul> */}
+            <Route component={LoginPage} exact path="/" />
+            {/* <Route component={LoginPage} exact path="/" /> */}
+            {/* <Route component={UserProfilePage} exact path="/my-profile" /> */}
+            <Route path='/my-profile'>
+              <UserProfilePage />
+            </Route>
+            <Route path='/search-items'>
+              <SearchItems />
+            </Route>
+            <Route path='/item-detail/:itemId'>
+              <DetailItem />
+            </Route>
+            {/* <h1>
+              APP.JS
+            </h1>
+            <LoginPage/> */}
+          </AuthContext.Provider>
         </Router>
       </div>
     );
-  }
 }
 
 export default App;
